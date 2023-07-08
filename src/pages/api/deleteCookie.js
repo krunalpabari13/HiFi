e912aaa50724481d9ecf1997955eee9196507d7d
krunal <krunalpabari13>
2023-07-09 01:29:06 +0530

    deletecookie

--- src/pages/api/deleteCookie.js
+++ src/pages/api/deleteCookie.js
@@ -1,10 +1,11 @@
 import { serialize } from "cookie";
 export default function logout(req,res){
+    const hostname=req.headers.host;
 const serailizeCookie=    serialize('jwt',null,{
         httpOnly:true,
         secure:true,
         expires:new Date(0),
-        domain:'localhost',
+        domain:hostname,
         path:'/'
     })
     res.setHeader('Set-Cookie',serailizeCookie);
